e0b3671cbcaf9839b4821082be4051786a257cdf
seroj-chobanyan <chobanyanserge@gmail.com>
2025-02-15 22:54:54 +0400

    install

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app =express();
+
+app.get('/hello', (req, res) => {
+
+
+    
+    res.send('Helllo!');
+});
+
+app.listen(8000, () => {
+    console.log('Server is listening on port 8000')
+})
